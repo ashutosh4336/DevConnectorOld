2ad71308ee0da308b15740b5e15805ef9f83d928
Ashutosh Panda <ashu.juga99@gmail.com>
2019-05-07 16:22:56 +0530

    MongoDB Connected

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+
+    console.log('MongoDB Connceted');
+  } catch (err) {
+    console.error(err.msg);
+    //Exit application
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
