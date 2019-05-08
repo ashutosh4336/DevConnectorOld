3cb7d3194270b734affe3d15758d296a2810ef3a
Ashutosh Panda <ashu.juga99@gmail.com>
2019-05-09 00:05:23 +0530

    Initial DataRetrival

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+//Import User Model
+
+const User = require('../../models/User');
 
 // @route  GET api/auth
 // @desc   Test Route
 // @access Public
-router.get('/', (req, res) => {
-  res.send('Auth Route');
+router.get('/', auth, async (req, res) => {
+  // res.send('Auth Route');
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
