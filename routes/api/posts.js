6e15547afa7e9033855af3d5adc9503c1b1ac6bf
Ashutosh Panda <ashu.juga99@gmail.com>
2019-05-10 01:46:25 +0530

    Get AllPost and PostByID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,9 +7,10 @@ const post = require('../../models/Post');
 const profile = require('../../models/Profile');
 const user = require('../../models/User');
 
-// @route  GET api/posts
+// @route  POST api/posts
 // @desc   Create a Post
-// @access Public
+// @access Private
+
 router.post(
   '/',
   [
@@ -45,5 +46,38 @@ router.post(
     }
   }
 );
+
+// @route  GET api/posts
+// @desc   GET all a Post
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/posts/:id
+// @desc   GET single Post by id
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ msg: 'Post Not Found' });
+
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId')
+      return res.status(404).json({ msg: 'Post Not Found' });
+
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
