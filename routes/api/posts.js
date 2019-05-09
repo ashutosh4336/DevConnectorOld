ad15f5b6ac6b4e1d8963d42f6b6af4bc383712cf
Ashutosh Panda <ashu.juga99@gmail.com>
2019-05-10 02:02:02 +0530

    Delete Post By ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -80,4 +80,33 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route  DELETE api/posts/:id
+// @desc   DELETE a Post
+// @access Private
+
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: 'Post Not Found' });
+
+    //Check on User
+    if (post.user.toString() !== req.user.id)
+      return res
+        .status(401)
+        .json({ msg: 'User Not Authorized to Delete The Post' });
+
+    await post.remove();
+
+    return res.json({ msg: 'Post Removed' });
+
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId')
+      return res.status(404).json({ msg: 'Post Not Found' });
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
